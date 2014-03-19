e36d2db91eb83ab0ef5eed805a35c310e62412cc
Andy Casey <andycasey@gmail.com>
2014-03-19 11:00:46 +0000

    it compiles,.. but it doesn't feel right

--- homogenisation/intrinsic.py
+++ homogenisation/intrinsic.py
@@ -1,8 +1,12 @@
+from pystan import stan, StanModel
+
 model_code = """
 data {
 
-    // Nodes named alpha, beta, ...
+    // Dimensionality (teff, logg, etc)
+    int<lower=1> N_dim;
 
+    // Nodes named alpha, beta, ...
     int<lower=2> N_bm_alpha;
     real alpha_teff_measured[N_bm_alpha];
     real<lower=0> alpha_teff_uncertainty[N_bm_alpha];
@@ -35,6 +39,10 @@ parameters {
     real<lower=0,upper=2> m_logg_alpha;
     real<lower=-5,upper=5> b_logg_alpha;
 
+    // Non-spectroscopic measured distributions for alpha node
+    real ns_teff_alpha[N_bm_alpha];
+    real ns_logg_alpha[N_bm_alpha];
+
     // The measured distributions of teff and logg by alpha node
     real teff_alpha[N_bm_alpha];
     real logg_alpha[N_bm_alpha];
@@ -44,6 +52,10 @@ parameters {
     real<lower=-1000,upper=1000> b_teff_beta;
     real<lower=0,upper=2> m_logg_beta;
     real<lower=-5,upper=5> b_logg_beta;
+
+    // Non-spectroscopic measured distributions for beta node
+    real ns_teff_beta[N_bm_beta];
+    real ns_logg_beta[N_bm_beta];
 
     // The measured distributions of teff and logg by beta node
     real teff_beta[N_bm_beta];
@@ -59,8 +71,9 @@ parameters {
     real<lower=0> s_beta_teff;
     real<lower=0> s_beta_logg;
 
-    // Latent outlier variable
-    real<lower=0,upper=1> p_0;
+    // Latent outlier variable p0 (avoiding alpha, gamma, etc as they
+    // refer to nodes)
+    real<lower=0,upper=1> p0;
     
     // Model background
     real<lower=0> mu_background_teff;
@@ -71,53 +84,141 @@ parameters {
 }
 transformed parameters {
     // Transform onto non-spectroscopic scale
-    real transformed_teff_alpha[N_bm_alpha];
-    real transformed_logg_alpha[N_bm_alpha];
+    real sp_teff_alpha[N_bm_alpha];
+    real sp_logg_alpha[N_bm_alpha];
 
-    real transformed_teff_beta[N_bm_beta];
-    real transformed_logg_beta[N_bm_beta];
+    real sp_teff_beta[N_bm_beta];
+    real sp_logg_beta[N_bm_beta];
 
     // Alpha node
-    for(i in 1:N_bm_alpha)
-      transformed_teff_alpha[i] <- m_teff_alpha*teff_alpha[i] + b_teff_alpha
-      transformed_logg_alpha[i] <- m_logg_alpha*logg_alpha[i] + b_logg_alpha
+    for (i in 1:N_bm_alpha)
+    {
+      sp_teff_alpha[i] <- m_teff_alpha*teff_alpha[i] + b_teff_alpha;
+      sp_logg_alpha[i] <- m_logg_alpha*logg_alpha[i] + b_logg_alpha;
+    }
 
     // Beta node
-    for(i in 1:N_bm_beta)
-      transformed_teff_beta[i] <- m_teff_beta*teff_beta[i] + b_teff_beta
-      transformed_logg_beta[i] <- m_logg_beta*logg_beta[i] + b_logg_beta
+    for (i in 1:N_bm_beta)
+    {
+      sp_teff_beta[i] <- m_teff_beta*teff_beta[i] + b_teff_beta;
+      sp_logg_beta[i] <- m_logg_beta*logg_beta[i] + b_logg_beta;
+    }
 }
 model {
 
+    // Outlier variables
+    real log_p0;
+    real log1m_p0;
+
+    // Create our vectors and matrices. Currently N_dim assumed to be 2 (teff, logg),
+    // but this can be generalised
+    vector[N_dim] ns_measurements;
+    vector[N_dim] sp_measurements;
+    matrix[N_dim,N_dim] covariance;
+    vector[N_dim] sp_correlated_measurements;
+
     // Non-spectroscopic measurements
-    ns_teff_alpha ~ normal(teff_ns_measured_alpha, teff_ns_uncertainty_alpha);
-    ns_teff_beta ~ normal(teff_ns_measured_beta, teff_ns_uncertainty_beta);
+    ns_teff_alpha ~ normal(ns_measured_teff_alpha, ns_uncertainty_teff_alpha);
+    ns_teff_beta ~ normal(ns_measured_teff_beta, ns_uncertainty_teff_beta);
 
-    logg_true_alpha ~ normal(logg_ns_measured_alpha, logg_ns_uncertainty_alpha);
-    logg_true_beta ~ normal(logg_ns_measured_beta, logg_ns_uncertainty_beta);
+    ns_logg_alpha ~ normal(ns_measured_logg_alpha, ns_uncertainty_logg_alpha);
+    ns_logg_beta ~ normal(ns_measured_logg_beta, ns_uncertainty_logg_beta);
 
-    // Node dispersions
+    // Node measurement variances
     s_alpha_teff ~ normal(0, 1e2);
     s_beta_teff ~ normal(0, 1e2);
 
     s_alpha_logg ~ normal(0, 1);
     s_beta_logg ~ normal(0, 1);
 
-    // Intrinsic dispersions
+    // Intrinsic variances
     s_intrinsic_teff ~ normal(0, 1e2);
     s_intrinsic_logg ~ normal(0, 1);
 
-    // Background
-    background_teff ~ normal(mu_background_teff, sigma_background_teff);
-    background_logg ~ normal(mu_background_logg, sigma_background_logg);
+    // Measurements (EXAMPLE ONLY -- this is for if we were not using a covariance matrix)
+    //y_teff_alpha ~ normal(sp_teff_alpha, pow(alpha_teff_uncertainty, 2) + s_alpha_teff + s_intrinsic_teff);
+    //y_teff_beta ~ normal(sp_teff_beta, pow(beta_teff_uncertainty, 2) + s_beta_teff + s_intrinsic_teff);
 
-    // Measurements
-    y_teff_alpha ~ normal(mu_teff_alpha, teff_uncertainty_alpha + eps_teff_alpha + eps_intrinsic_teff)
-    y_teff_beta ~ normal(mu_teff_beta, teff_uncertainty_beta + eps_teff_beta + eps_intrinsic_teff)
+    //y_logg_alpha ~ normal(sp_logg_alpha, pow(logg_uncertainty_alpha, 2) + s_alpha_logg + s_intrinsic_logg);
+    //y_logg_beta ~ normal(sp_logg_beta, pow(logg_uncertainty_beta, 2) + s_beta_logg + s_intrinsic_logg);
 
-    y_logg_alpha ~ normal(mu_logg_alpha, logg_uncertainty_alpha + eps_logg_alpha + eps_intrinsic_logg)
-    y_logg_beta ~ normal(mu_logg_beta, logg_uncertainty_beta + eps_logg_beta + eps_intrinsic_logg)
+    {
+        // Outlier variables
+        log_p0 <- log(p0);
+        log1m_p0 <- log1m(p0);
 
+        // Do alpha node first
+        for (i in 1:N_bm_alpha)
+        {
+            // Non-spectroscopic measurements
+            ns_measurements[1] <- ns_teff_alpha[i];
+            ns_measurements[2] <- ns_logg_alpha[i];
+
+            // Transformed spectroscopic measurements for alpha node
+            sp_measurements[1] <- sp_teff_alpha[i];
+            sp_measurements[2] <- sp_logg_alpha[i];
+
+            // Covariance matrix
+            // Consider the simple case with no covariance:
+            covariance[1,2] <- 0;
+            covariance[2,1] <- 0;
+            covariance[1,1] <- pow(alpha_teff_uncertainty[i], 2) + s_alpha_teff + s_intrinsic_teff;
+            covariance[2,2] <- pow(alpha_logg_uncertainty[i], 2) + s_alpha_logg + s_intrinsic_logg;
+
+            sp_correlated_measurements ~ multi_normal(sp_measurements, covariance);
+
+            // Adding these separately,.. doesn't feel right.
+            increment_log_prob(log_sum_exp(
+                 log_p0  + normal_log(ns_measurements, sp_correlated_measurements[1], pow(alpha_teff_uncertainty[i], 2) + s_alpha_teff + s_intrinsic_teff),
+                 
+                // Not considering covariance in background measurements -- should we be applying the
+                // same covariance matrix to the background parameters?
+                log1m_p0 + normal_log(ns_teff_alpha[i], mu_background_teff, sigma_background_teff)
+            ));
+            
+            increment_log_prob(log_sum_exp(
+                 log_p0  + normal_log(ns_measurements, sp_correlated_measurements[2], pow(alpha_logg_uncertainty[i], 2) + s_alpha_logg + s_intrinsic_logg),
+                log1m_p0 + normal_log(ns_logg_alpha[i], mu_background_logg, sigma_background_logg)
+            ));
+        }
+
+        // Repeat for beta node
+        for (i in 1:N_bm_beta)
+        {
+            // Non-spectroscopic measurements
+            ns_measurements[1] <- ns_teff_beta[i];
+            ns_measurements[2] <- ns_logg_beta[i];
+
+            // Transformed spectroscopic measurements for beta node
+            sp_measurements[1] <- sp_teff_beta[i];
+            sp_measurements[2] <- sp_logg_beta[i];
+
+            // Covariance matrix
+            // Consider the simple case with no covariance:
+            covariance[1,2] <- 0;
+            covariance[2,1] <- 0;
+            covariance[1,1] <- pow(beta_teff_uncertainty[i], 2) + s_beta_teff + s_intrinsic_teff;
+            covariance[2,2] <- pow(beta_logg_uncertainty[i], 2) + s_beta_logg + s_intrinsic_logg;
+
+            sp_correlated_measurements ~ multi_normal(sp_measurements, covariance);
+
+            // Adding these separately,.. doesn't feel right.
+            increment_log_prob(log_sum_exp(
+                 log_p0  + normal_log(ns_measurements, sp_correlated_measurements[1], pow(beta_teff_uncertainty[i], 2) + s_beta_teff + s_intrinsic_teff),
+                 
+                // Not considering covariance in background measurements -- should we be applying the
+                // same covariance matrix to the background parameters?
+                log1m_p0 + normal_log(ns_teff_beta[i], mu_background_teff, sigma_background_teff)
+            ));
+            
+            increment_log_prob(log_sum_exp(
+                 log_p0  + normal_log(ns_measurements, sp_correlated_measurements[2], pow(beta_logg_uncertainty[i], 2) + s_beta_logg + s_intrinsic_logg),
+                log1m_p0 + normal_log(ns_logg_beta[i], mu_background_logg, sigma_background_logg)
+            ));
+        }
+    }
 
 }
 """
+
+sm = StanModel(model_code=model_code)
